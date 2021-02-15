b2366c0ecaa0d1617a519b2dc6d819bddb1fd063
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-02-15 17:48:48 +0100

    Update Util_classes.py

--- Project/Util/Util_classes.py
+++ Project/Util/Util_classes.py
@@ -9,3 +9,33 @@
 
 This contains all the classes used in the project.
 '''
+
+import Util_functions as Uf
+import threading
+import os
+
+
+class RequestThread(threading.Thread):
+    '''< Thread > object which performs a request (.start method) with the 
+    provided '.url'. Then it stores the data in '.data' attribute.'''
+    def __init__(self, url):
+        super().__init__()
+        self.data = None     # stored data after request
+        self.url = url       # URL with contains the content of the request
+
+    def run(self, *args, **kargs):
+        '''Makes the URL request and stores the data in '.data' attribute.'''
+        self.data = Uf.handleRequest(self.url, *args, **kargs)
+    
+    def writeFILE(self, path):
+        '''Writes the data collected by the thread in a file identified by 
+        'path'.'''
+        with open(path, 'wb') as f:
+            # Write the content of the data
+            f.write(self.data.content)
+    
+    def __repr__(self):
+        out = f'''Parent PID: {os.getppid()}\nProcess PID: {os.getpid()}\nAttributes:
+    -> .url: {self.url} ({type(self.url)})
+    -> .data: {self.data} ({type(self.data)})'''
+        return out
