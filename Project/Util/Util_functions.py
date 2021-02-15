123ba5d85bf2eebee6d996ad72bbb4880cf7527e
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-02-15 17:50:52 +0100

    Update Util_functions.py

--- Project/Util/Util_functions.py
+++ Project/Util/Util_functions.py
@@ -9,3 +9,20 @@
 
 This contains all the functions used in the project.
 '''
+
+import requests
+import os
+
+
+def handleRequest(url, *args, **kargs):
+    '''Returns the data gathered by the 'url' request.'''
+    try:
+        # Get data from URL request
+        data = requests.get(url, *args, **kargs)
+        # If the response was successful, no Exception will be raised
+        data.raise_for_status()
+    except Exception as exception:
+        print(f'\nAn exception occurred: {str(exception)}. Process PID: {os.getpid()}')
+    else:
+        print(f'\nSuccess! Process id: {os.getpid()}')
+    return data
