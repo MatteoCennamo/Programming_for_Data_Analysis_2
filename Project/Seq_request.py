e6e6b59461cd6e8664e71c930fb2b807cd263be6
matimore <79093303+matimore@users.noreply.github.com>
2021-02-16 18:29:46 +0100

    Update Seq_request.py

--- Project/Seq_request.py
+++ Project/Seq_request.py
@@ -11,3 +11,37 @@ Sequantial vesion of the API request.
 '''
 
 from Util import Util_functions as Uf
+
+
+# file da cui ho visto come fare l'url 
+https://mixedanalytics.com/knowledge-base/access-open-weather-api-data-in-google-sheets/
+https://mixedanalytics.com/knowledge-base/import-financial-modeling-prep-stock-data-google-sheets/
+
+ 
+ #tentativo 1
+ 
+import requests
+import time
+
+cities = ['London', 'Tokyo']
+
+urls = []
+for city in cities:
+    urls.append('api.openweathermap.org/data/2.5/weather'
+                + city +
+                'appid=23df90be877fed80721f131eafff5c6a')
+start = time.time()
+for url in urls:
+    try:
+        response = requests.get(url)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+        print(response.text)
+    except Exception as exception:
+        print('An exception occurred: ' + str(exception))
+    else:
+        print('Success!')
+
+end = time.time()
+print('Took %.3f seconds' % (end - start))
+  
