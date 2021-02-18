f73f8bc32c39086f7d9e26efcb23181a729c04d8
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-02-18 11:59:39 +0100

    Update MT_request.py

--- Project/MT_request.py
+++ Project/MT_request.py
@@ -11,52 +11,49 @@ Multi-Threads version of the API request. It downloads the data
 into < Assets/JSON_files > folder in .json format.
 '''
 import time
-import requests
-import os
-import threading
-import json
-
-##   multithreading
-
-class Url_Thread(threading.Thread):
-    def __init__(self,url,city):
-        super().__init__()
-        self.city = city
-        self.url = url
+import memory_profiler
+from Util import Util_classes as Uc
 
 
-    def run(self):
-        try:
-            response = requests.get(self.url)
-            response.raise_for_status()
-            print (response.text)
-            with open(self.city + ".json", "w") as write_file:
-                #json.dump(response.text, write_file, indent = 6)
-                write_file.write(response.text)
-        except Exception as exception:
-            print ( 'An exception occurred: ' + str (exception))
-        else :
-            print ( 'Success!' )
-
-
-CITIES = ['Toronto','New York City','Rio de Janeiro','Buenos Aires','Nuuk','London','Rome','Oslo','Cairo',
-          'Dubai','Moscow','Yakutsk','Cape Town','Nairobi','Tehran','New Delhi','Sydney','Honolulu', 'Tokyo','Beijing']
-
+# Cities and API-key definition
+CITIES = ['Toronto','New York City','Rio de Janeiro','Buenos Aires','Nuuk', 
+          'London','Rome','Oslo','Cairo','Dubai','Moscow','Yakutsk','Cape Town', 
+          'Nairobi','Tehran','New Delhi','Sydney','Honolulu', 'Tokyo','Beijing',]
 APIKEY = '23df90be877fed80721f131eafff5c6a'
 
-urls = []
-for city in CITIES:
-    urls.append(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}')
-        
-start = time.time()
+def main():
+    # Create list of URLs and cities
+    urls = []
+    for city in CITIES:
+        urls.append((f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}', city))
+    # Create threads and launch them
+    threads = []
+    for url in urls:
+        thread = Uc.RequestThread(*url)
+        thread.start() 
+        threads.append(thread)
+    # Join the threads
+    for thread in threads:
+        thread.join()
+    
+    # Write the data in .json files
+    for thread in threads:
+        thread.writeFILE()
 
-threads = []
-for i in range(len(urls)):
-    thread = Url_Thread(urls[i],CITIES[i])
-    thread.start() 
-    threads.append(thread)
-
-for thread in threads:
-    thread.join()
-end = time.time()
-print('Took %.3f seconds' % (end - start))
+if __name__ == '__main__':
+    # Memory before program call
+    m1 = memory_profiler.memory_usage()
+    # Time before process
+    start = time.process_time()
+    
+    main()
+    
+    # Time after process
+    end = time.process_time()
+    # Memory after program call
+    m2 = memory_profiler.memory_usage()
+    # Compute memory usage and processing time
+    time_diff = end - start
+    mem_diff = m2[0] - m1[0]
+    print("It took {:2.4} Secs and {:2.3} Mb to execute this program.".format(
+            time_diff, mem_diff))
