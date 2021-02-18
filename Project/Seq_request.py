d4e77891ce6f74c8b9736f2afefdd1f89d468701
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-02-18 11:16:27 +0100

    Update Seq_request.py

--- Project/Seq_request.py
+++ Project/Seq_request.py
@@ -15,36 +15,41 @@ import memory_profiler
 from Util import Util_functions as Uf
 
 
-# Memory before method call
-m1 = memory_profiler.memory_usage()
+def main():
+    # Cities and API-key definition
+    CITIES = ['Toronto','New York City','Rio de Janeiro','Buenos Aires','Nuuk', 
+              'London','Rome',' Oslo','Cairo', 'Dubai','Moscow','Yakutsk', 
+              'Cape Town','Nairobi','Tehran','New Delhi','Sydney','Honolulu', 
+              'Tokyo','Beijing']
+    APIKEY = '23df90be877fed80721f131eafff5c6a'
+    
+    # Create list of URLs
+    urls = []
+    for city in CITIES:
+        urls.append(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}')
+    
+    # Initialize LIST of data
+    responses = []
+    
+    # Process the queries
+    for url in urls:
+        responses += [Uf.handleRequest(url)]
 
-# Cities and API-key definition
-CITIES = ['Toronto','New York City','Rio de Janeiro','Buenos Aires','Nuuk','London','Rome',' Oslo','Cairo',
-          'Dubai','Moscow','Yakutsk','Cape Town','Nairobi','Tehran','New Delhi','Sydney','Honolulu', 'Tokyo','Beijing',]
-APIKEY = '23df90be877fed80721f131eafff5c6a'
-
-# Create list of URLs
-urls = []
-for city in CITIES:
-    urls.append(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}')
-
-# Initialize LIST of data
-responses = []
-
-# Time before process
-start = time.process_time()
-
-# Process the queries
-for url in urls:
-    responses += [Uf.handleRequest(url)]
-
-# Time after process
-end = time.process_time()
-
-# Memory after method call
-m2 = memory_profiler.memory_usage()
-
-# Compute memory usage and processing time
-time_diff = end - start
-mem_diff = m2[0] - m1[0]
-print("It took {:2.3} Secs and {:2.3} Mb to execute this program.".format(time_diff, mem_diff))
+if __name__ == '__main__':
+    # Memory before program call
+    m1 = memory_profiler.memory_usage()
+    # Time before process
+    start = time.process_time()
+    
+    main()
+    
+    # Time after process
+    end = time.process_time()
+    # Memory after program call
+    m2 = memory_profiler.memory_usage()
+    
+    # Compute memory usage and processing time
+    time_diff = end - start
+    mem_diff = m2[0] - m1[0]
+    print("It took {:2.4} Secs and {:2.3} Mb to execute this program.".format(
+            time_diff, mem_diff))
