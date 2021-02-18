0dd87d910e25b37744806581aaef3a97f1bc9ec7
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-02-18 14:44:42 +0100

    Update MT_day_pollution.py

--- Project/MT_day_pollution.py
+++ Project/MT_day_pollution.py
@@ -2,9 +2,9 @@
  
 
  
-http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API key}
+# http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API key}
  
- import time
+import time
 import memory_profiler
 from Util import Util_classes as Uc
 
